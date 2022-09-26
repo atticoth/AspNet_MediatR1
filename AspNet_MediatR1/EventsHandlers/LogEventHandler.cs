cf6a524a70029f059dca3f4e110a10530fe97ac3
Thiago Attico da Silva <thiago.attico@itlab.com.br>
2022-09-26 11:21:44 -0300

    Log das notificações

---
+++ AspNet_MediatR1/EventsHandlers/LogEventHandler.cs
@@ -0,0 +1,50 @@
+﻿using AspNet_MediatR1.Notification;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspNet_MediatR1.EventsHandlers
+{
+    public class LogEventHandler :
+                            INotificationHandler<ProdutoCreateNotification>,
+                            INotificationHandler<ProdutoUpdateNotification>,
+                            INotificationHandler<ProdutoDeleteNotification>,
+                            INotificationHandler<ErroNotification>
+    {
+        public Task Handle(ProdutoCreateNotification notification, CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine($"CRIACAO: '{notification.Id} " +
+                    $"- {notification.Nome} - {notification.Preco}'");
+            });
+        }
+
+        public Task Handle(ProdutoUpdateNotification notification, CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine($"ALTERACAO: '{notification.Id} - {notification.Nome} " +
+                    $"- {notification.Preco} - {notification.IsConcluido}'");
+            });
+        }
+
+        public Task Handle(ProdutoDeleteNotification notification, CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine($"EXCLUSAO: '{notification.Id} " +
+                    $"- {notification.IsConcluido}'");
+            });
+        }
+
+        public Task Handle(ErroNotification notification, CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine($"ERRO: '{notification.Erro} \n {notification.PilhaErro}'");
+            });
+        }
+    }
+}
