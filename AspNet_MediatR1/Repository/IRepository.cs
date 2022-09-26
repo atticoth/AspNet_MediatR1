33ed29d8d93401e02181e1808d886da50a772320
Thiago Attico da Silva <thiago.attico@itlab.com.br>
2022-09-26 10:58:06 -0300

    Criando o Repositório

---
+++ AspNet_MediatR1/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspNet_MediatR1.Repository
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Get(int id);
+        Task Add(T item);
+        Task Edit(T item);
+        Task Delete(int id);
+    }
+}
---
+++ AspNet_MediatR1/Repository/ProdutoRepository.cs
@@ -0,0 +1,41 @@
+﻿using AspNet_MediatR1.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNet_MediatR1.Repository
+{
+    public class ProdutoRepository : IRepository<Produto>
+    {
+        private static Dictionary<int, Produto> _produtos = new Dictionary<int, Produto>();
+
+        public async Task<IEnumerable<Produto>> GetAll()
+        {
+            return await Task.Run(() => _produtos.Values.ToList());
+        }
+
+        public async Task<Produto> Get(int id)
+        {
+            return await Task.Run(() => _produtos.GetValueOrDefault(id));
+        }
+
+        public async Task Add(Produto produto)
+        {
+            await Task.Run(() => _produtos.Add(produto.Id, produto));
+        }
+
+        public async Task Edit(Produto produto)
+        {
+            await Task.Run(() =>
+            {
+                _produtos.Remove(produto.Id);
+                _produtos.Add(produto.Id, produto);
+            });
+        }
+
+        public async Task Delete(int id)
+        {
+            await Task.Run(() => _produtos.Remove(id));
+        }
+    }
+}
