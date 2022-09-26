9a52d42931f53185db7b668af71599ff93a9eeb8
Thiago Attico da Silva <thiago.attico@itlab.com.br>
2022-09-26 10:57:38 -0300

    Criando a Entidade

---
+++ AspNet_MediatR1/Entity/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNet_MediatR1.Entity
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+
+    }
+}
